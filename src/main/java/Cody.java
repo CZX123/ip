2c254a0cec26cbfd0dc49b7b561c67fc2d97811d
CZX <CZX123@users.noreply.github.com>
2025-08-22 14:37:20 +0800

    Add functionality to add and list tasks

--- src/main/java/Cody.java
+++ src/main/java/Cody.java
@@ -5,13 +5,28 @@ public class Cody {
         String welcomeMsg = "\n👋 Hello! I'm Cody. 🤖\nWhat can I do for you? 🌈\n";
         String goodbyeMsg = "👋 Bye. Hope to see you again soon! ✨";
         String divider = "⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯";
-        String indent = "    ";
+        String indent = "  ";
         System.out.println(welcomeMsg + "\n" + divider + "\n");
         Scanner input = new Scanner(System.in);
-        String inputTxt = input.next();
+        String inputTxt = input.nextLine();
+        String[] list = new String[100];
+        int listCount = 0;
         while (!inputTxt.equals("bye")) {
-            System.out.println("\n" + indent + inputTxt + "\n\n" + divider + "\n");
-            inputTxt = input.next();
+            if (inputTxt.equals("list")) {
+                System.out.println();
+                if (listCount == 0) System.out.println(indent + "You have no tasks for today! 😎");
+                else System.out.printf("%sYou have %d task%s! 💪📝\n", indent, listCount, listCount > 1 ? "s" : "");
+                for (int i = 0; i < listCount; i++) {
+                    System.out.printf("%s%d. %s\n", indent, i+1, list[i]);
+                }
+                System.out.println();
+            } else {
+                list[listCount] = inputTxt;
+                listCount++;
+                System.out.println("\n" + indent + "added: " + inputTxt
+                    + "\n\n" + divider + "\n");
+            }
+            inputTxt = input.nextLine();
         }
         System.out.println("\n" + divider + "\n\n" + goodbyeMsg);
     }
