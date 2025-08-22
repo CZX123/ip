d94dfc0b7a629634eb9dea81fb4b2e72b594723a
CZX <CZX123@users.noreply.github.com>
2025-08-22 14:52:13 +0800

    Refactor code

--- src/main/java/Cody.java
+++ src/main/java/Cody.java
@@ -1,33 +1,41 @@
 import java.util.Scanner;
 
 public class Cody {
+    private static final String WELCOME_MSG = "\n👋 Hello! I'm Cody. 🤖\nWhat can I do for you? 🌈\n";
+    private static final String GOODBYE_MSG = "👋 Bye. Hope to see you again soon! ✨";
+    private static final String DIVIDER = "\n⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n";
+    private static final String INDENT = "  ";
+    private static final Scanner input = new Scanner(System.in);
+    private static final String[] tasks = new String[100];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
-        String welcomeMsg = "\n👋 Hello! I'm Cody. 🤖\nWhat can I do for you? 🌈\n";
-        String goodbyeMsg = "👋 Bye. Hope to see you again soon! ✨";
-        String divider = "⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯";
-        String indent = "  ";
-        System.out.println(welcomeMsg + "\n" + divider + "\n");
-        Scanner input = new Scanner(System.in);
+        System.out.println(WELCOME_MSG + DIVIDER);
+
         String inputTxt = input.nextLine();
-        String[] list = new String[100];
-        int listCount = 0;
+
         while (!inputTxt.equals("bye")) {
             if (inputTxt.equals("list")) {
                 System.out.println();
-                if (listCount == 0) System.out.println(indent + "You have no tasks for today! 😎");
-                else System.out.printf("%sYou have %d task%s! 💪📝\n", indent, listCount, listCount > 1 ? "s" : "");
-                for (int i = 0; i < listCount; i++) {
-                    System.out.printf("%s%d. %s\n", indent, i+1, list[i]);
+                if (taskCount == 0) {
+                    System.out.println(INDENT + "You have no tasks for today! 😎");
                 }
-                System.out.println();
+                else {
+                    System.out.printf("%sYou have %d task%s! 💪📝\n",
+                                      INDENT, taskCount, taskCount > 1 ? "s" : "");
+                }
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.printf("%s%d. %s\n", INDENT, i+1, tasks[i]);
+                }
+                System.out.println(DIVIDER);
             } else {
-                list[listCount] = inputTxt;
-                listCount++;
-                System.out.println("\n" + indent + "added: " + inputTxt
-                    + "\n\n" + divider + "\n");
+                tasks[taskCount] = inputTxt;
+                taskCount++;
+                System.out.println("\n" + INDENT + "added: " + inputTxt + "\n" + DIVIDER);
             }
             inputTxt = input.nextLine();
         }
-        System.out.println("\n" + divider + "\n\n" + goodbyeMsg);
+
+        System.out.println(DIVIDER + "\n" + GOODBYE_MSG);
     }
 }
