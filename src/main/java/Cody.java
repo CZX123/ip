48f16acca700cb99b10e9edb535188dc6364a896
CZX <CZX123@users.noreply.github.com>
2025-08-22 14:09:56 +0800

    Add echo functionality

--- src/main/java/Cody.java
+++ src/main/java/Cody.java
@@ -1,16 +1,18 @@
+import java.util.Scanner;
+
 public class Cody {
     public static void main(String[] args) {
-        String welcomeMsg = """
-            
-             
-             👋 Hello! I'm Cody. 🤖
-             What can I do for you? 🌈
-             
-            ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
-            
-             👋 Bye. Hope to see you again soon! ✨
-            
-            """;
-        System.out.println(welcomeMsg);
+        String welcomeMsg = "\n👋 Hello! I'm Cody. 🤖\nWhat can I do for you? 🌈\n";
+        String goodbyeMsg = "👋 Bye. Hope to see you again soon! ✨";
+        String divider = "⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯";
+        String indent = "    ";
+        System.out.println(welcomeMsg + "\n" + divider + "\n");
+        Scanner input = new Scanner(System.in);
+        String inputTxt = input.next();
+        while (!inputTxt.equals("bye")) {
+            System.out.println("\n" + indent + inputTxt + "\n\n" + divider + "\n");
+            inputTxt = input.next();
+        }
+        System.out.println("\n" + divider + "\n\n" + goodbyeMsg);
     }
 }
