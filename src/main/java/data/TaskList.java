a608a2b6a415c31061e3d593e9f91f808a47e155
CZX <CZX123@users.noreply.github.com>
2025-08-29 12:54:14 +0800

    Cleanup code

--- src/main/java/data/TaskList.java
+++ src/main/java/data/TaskList.java
@@ -2,7 +2,6 @@ package data;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Iterator;
 import java.util.function.Consumer;
